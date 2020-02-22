93b34f83592bde0332f8b12a6f228df5b5cd5764
tan <nttrongtan1605@gmail.com>
2020-02-21 21:51:52 -0500

    Updates

---
+++ precache-manifest.d12d69b5222648765b81180507c78115.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07d941993a2e00b0d895a979719796db",
+    "url": "/demoreact/index.html"
+  },
+  {
+    "revision": "cb5979c3d98e1299e208",
+    "url": "/demoreact/static/css/main.c5d9544f.chunk.css"
+  },
+  {
+    "revision": "1c2cdb062ddea4ffd57d",
+    "url": "/demoreact/static/js/2.2e848659.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/demoreact/static/js/2.2e848659.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cb5979c3d98e1299e208",
+    "url": "/demoreact/static/js/main.3a5e9e6e.chunk.js"
+  },
+  {
+    "revision": "3770bda9b6319c6498dd",
+    "url": "/demoreact/static/js/runtime-main.f98f1ef0.js"
+  }
+]);
